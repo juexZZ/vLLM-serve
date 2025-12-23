bb53e767bd85bd16a8d6463ec56acdd601036777
juexZZ <954742885@qq.com>
2025-12-23 06:40:23 -0500

    update test to avoid token len error.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # after server is hosted on port 8000
 from openai import OpenAI
 # Set OpenAI's API key and API base to use vLLM's API server.
-openai_api_key = "EMPTY"
+openai_api_key = "ai4ce"
 openai_api_base = "http://localhost:8000/v1"
 
 client = OpenAI(
@@ -14,7 +14,6 @@ chat_response = client.chat.completions.create(
     messages=[
         {"role": "user", "content": "Give me a short introduction to large language models."},
     ],
-    max_tokens=32768,
     temperature=0.6,
     top_p=0.95,
     extra_body={
